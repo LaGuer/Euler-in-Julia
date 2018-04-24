0f4c354b52e393a8bea81de9906d43b06faacf92
SundaraRaman R <sundar@abiteasier.in>
2018-04-25 02:57:01 +0530

    add jl program to find LCM(1,2,3,...,N)

---
+++ 5_lcm_upto.jl
@@ -0,0 +1,17 @@
+
+function lcm_upto(l)
+    #easy way: lcm(1:l)
+
+    #hard way: (~50 times slower than easy way)
+    mult = 1
+    nums = collect(1:l)
+    for idx in 2:length(nums)
+        n = nums[idx]
+        mult *= n
+
+        #divide away current number from any other multiples of it
+        nums[nums .% n .== 0] ./= n
+    end
+    mult
+end
+
