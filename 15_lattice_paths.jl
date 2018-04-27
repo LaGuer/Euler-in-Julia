ae1eef20c5355dc7946c6645d16c115532d35b4a
SundaraRaman R <sundar@abiteasier.in>
2018-04-28 00:14:42 +0530

    add jl prog to find grid paths by recursion

---
+++ 15_lattice_paths.jl
@@ -0,0 +1,34 @@
+
+
+function num_downright_paths(gridsz = 20)
+    # grid size in problem spec refers to number of edges, so no. of pts is +1 that
+    grid = fill(-1, gridsz+1, gridsz+1)
+
+    num_routes_to_end!(grid, 1, 1)
+end
+
+function num_routes_to_end!(grid, x, y)
+    grid[x, y] ≥ 0 && return grid[x, y]
+
+    last_idx = size(grid, 1)
+    if (x, y) == (last_idx, last_idx)
+        grid[x, y] = 1 #1 route from last point to itself
+        return grid[x, y]
+    end
+
+    down_routes = right_routes = 0
+    if x != last_idx
+        down_routes = num_routes_to_end!(grid, x+1, y)
+    end
+    if y != last_idx
+        right_routes = num_routes_to_end!(grid, x, y+1)
+    end
+
+    total_routes = down_routes + right_routes
+    grid[x, y] = total_routes
+end
+
+if !isinteractive()
+    println(num_downright_paths())
+end
+
