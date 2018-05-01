f9580918d2265ed5ce687c47e4870664b6b31379
SundaraRaman R <sundar@abiteasier.in>
2018-05-01 20:18:40 +0530

    add comments to lex perm problem (24)

--- 24_nth_lex_perm.jl
+++ 24_nth_lex_perm.jl
@@ -9,16 +9,21 @@ function lex_perm_at_pos(digs::AbstractArray = collect(0:9), pos::Integer = 1_00
     ndigs::Int = length(digs)
     digs_to_num(ds)::Int = sum([d*10^(ndigs-i) for (i, d) in enumerate(ds)])
 
+    #the sorted order is position 1 in lexicographic ordering
     pos == 1 && return digs_to_num(digs)
 
     pos_to_traverse = pos - 1 #4th pos means 3 to traverse from the sorted ordering
     perm_at_pos = copy(digs)
     for i in 1:length(perm_at_pos)
+        #total number of permutations of digits beyond this placevalue
         perms_under_dig = factorial(ndigs - i)
+        #cross n digits in this position = traverse n*perms_under_dig positions in lex order
         digits_to_cross = pos_to_traverse ÷ perms_under_dig
 
         perm_at_pos[i] = digs[digits_to_cross+1]
         deleteat!(digs, digits_to_cross+1)
+        #digits_to_cross*perms_under_dig positions have been traversed by setting this digit, 
+        # so subtract that away (using modulus)
         pos_to_traverse %= perms_under_dig
     end
 
