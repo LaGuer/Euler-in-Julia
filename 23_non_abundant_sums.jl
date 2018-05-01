0f1202ceb33bfd2bb9a636fb120157f1f5d257fd
SundaraRaman R <sundar@abiteasier.in>
2018-05-01 12:23:35 +0530

    simplify non-abundant-sums (slightly slower, a lot more memory efficient)

--- 23_non_abundant_sums.jl
+++ 23_non_abundant_sums.jl
@@ -1,24 +1,23 @@
 
 
-using Primes: factor
-
-const ABUNDANT_SUM_BEGIN = 28123
+const ABUNDANT_SUM_LIMIT = 28123
 
 function non_abundant_sums()
     #Total value of non-abundant-sum numbers
     non_abundant_sum_total = sum(1:23)
+    abundant_nums = get_abundant_nums_upto(ABUNDANT_SUM_LIMIT)
 
-    for n in 25:ABUNDANT_SUM_BEGIN
-        if !is_sum_of_abundants(n)
+    for n in 25:ABUNDANT_SUM_LIMIT
+        if !is_sum_of_pair(n, abundant_nums)
             non_abundant_sum_total += n
         end
     end
     non_abundant_sum_total 
 end
 
-function is_sum_of_abundants(n)
-    for a in get_abundant_nums_upto(n÷2)
-        if is_abundant(n - a)
+function is_sum_of_pair(n, narray)
+    for i in narray
+        if (n - i) in narray
             return true
         end
     end
@@ -26,38 +25,24 @@ function is_sum_of_abundants(n)
     return false
 end
 
-let abundant_nums = Int[], prev_limit::Int = zero(Int)
-    global get_abundant_nums_upto, is_abundant
-    is_abundant(n) = (n in abundant_nums) || (sum(proper_divisors(n)) > n)
+@inline is_abundant(n) = sum(proper_divisors(n)) > n
 
-    function get_abundant_nums_upto(limit::Int)
-        if prev_limit ≥ limit
-            return filter(n -> n < limit, abundant_nums)
+function get_abundant_nums_upto(limit::Int)
+    abundant_nums = Int[]
+    for n in 12:limit
+        if is_abundant(n)
+            push!(abundant_nums, n)
         end
-
-        for n in prev_limit:limit
-            if is_abundant(n)
-                push!(abundant_nums, n)
-            end
-        end
-
-        prev_limit = limit
-        abundant_nums
     end
+
+    abundant_nums
 end
 
 function proper_divisors(n::Int)
     0 ≤ n ≤ 1 && return Int[] 
 
-    pf = factor(n)
-    pdiv = ones(Int,1)
-    for (pr, exp) in pf
-        m = Int[pr^i for i in 0:exp]
-        pdiv = vec(pdiv * m')
-    end
-
-    sort!(pdiv)
-    pdiv[1:end-1]
+    lim = n÷2
+    pdiv = [d for d in 2:lim if n%d == 0]
 end
 
 if !isinteractive()
