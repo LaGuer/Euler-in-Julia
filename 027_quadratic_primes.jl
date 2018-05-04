4ee46bb632eacb3b6de217b40843dd2e77401d3d
SundaraRaman R <sundar@abiteasier.in>
2018-05-05 04:03:38 +0530

    027 move quad calc into main, add 6n+1 prelim check
    Almost twice as fast as previous version.
    Went from 390ms to 360ms on inlining function code,
    went from that to 190ms on adding the 6n+/-1 check

--- 027_quadratic_primes.jl
+++ 027_quadratic_primes.jl
@@ -4,28 +4,23 @@ using Primes: isprime
 function coeff_prod_prime_quad(alim = 1000, blim = 1000)
     local coeff_prod::Int
     max_np = 0
-    for a = -alim:alim, b = -blim:blim
-        np = num_primes_quad(a, b)
-        if np > max_np
+    for a = -alim:alim, b = 2:blim
+        # b should be prime, primes above 3 are all 6x-1 or 6x+1
+        (b ≤ 3 || (b-1)%6 == 0 || (b+1)%6 == 0) || continue
+
+        n = 0
+        quad_val = b #for n = 0, n² + an + b becomes b
+        while isprime(quad_val)
+            n += 1
+            quad_val = n^2 + a*n + b
+        end
+
+        if n > max_np
             coeff_prod = a * b
-            max_np = np
+            max_np = n
         end
     end
     coeff_prod
-end
-
-function num_primes_quad(a, b)
-    np = 0
-    n = 0
-    quad_val = b #for n = 0, n² + an + b becomes b
-
-    while isprime(quad_val)
-        np += 1
-        n += 1
-        quad_val = n^2 + a*n + b
-    end
-
-    np
 end
 
 if !isinteractive()
