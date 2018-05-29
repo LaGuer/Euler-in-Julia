ee9cdb7ba91ec316b05fa614396818a221e8bd0a
SundaraRaman R <sundar@abiteasier.in>
2018-05-29 19:01:46 +0530

    031 allow 0 xn solutions, hence remove Combinatorics

--- 031_coin_sums.jl
+++ 031_coin_sums.jl
@@ -1,15 +1,7 @@
 
 
-using Combinatorics
-
 function coin_sums_count(total = 200, denoms = [200, 100, 50, 20, 10, 5, 2, 1])
-    coin_combs = combinations(denoms)
-
-    counter = 0
-    for c in coin_combs
-        counter += num_dioph_solns(total, c)
-    end
-    counter
+    num_dioph_solns(total, c)
 end
 
 """
@@ -17,6 +9,7 @@ num_dioph_solns(t, C)
 Number of soluions of a given linear Diophantine equation of the form
 c1x1 + c2x2 + c3x3 + ... = t
 where [c1, c2, c3, ...] = C, and x1, x2, x3, ... are the unknowns to solve for.
+Allows any of xn to be zero.
 """
 function num_dioph_solns(total, coeffs)
     if length(coeffs) == 1
@@ -31,14 +24,12 @@ function num_dioph_solns(total, coeffs)
         #=
         x0 = u*(total÷d); y0 = v*(total÷d)
         x = x0 + (b/d)t; y = y0 - (a/d)t
-        => -dx0/b < t < dy0/a to have positive x and y
+        => -dx0/b ≤ t ≤ dy0/a to have positive x and y
         Number of solutions = number of t's in that range
-        -d*x0/b = (-d*u*total/d)/b = -u*total/b. Similarly with d*y0/a
+        -d*x0/b = (-d*u*total/d)/b = -u*total/b. Similarly, d*y0/a = v*total/a
         =#
-        upper_t_lim = (v*total/a)
-        upper_t_lim = isinteger(upper_t_lim) ? upper_t_lim - 1 : floor(upper_t_lim)
-        lower_t_lim = (-u*total/b)
-        lower_t_lim = isinteger(lower_t_lim) ? lower_t_lim + 1 : ceil(lower_t_lim)
+        upper_t_lim = floor(v*total/a)
+        lower_t_lim = ceil(-u*total/b)
         return length(lower_t_lim:upper_t_lim)
     end
 
