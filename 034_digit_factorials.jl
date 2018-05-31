86df5b32a053cc8b326f6ccaa4c041e5ab4c5142
SundaraRaman R <sundar@abiteasier.in>
2018-06-01 01:12:33 +0530

    034 4x faster using better upper limit
    To 1 s from 3.8 s

--- 034_digit_factorials.jl
+++ 034_digit_factorials.jl
@@ -12,7 +12,7 @@ function sum_digit_factorial_nums()
     That product for 8-digit numbers is factorial(9)*8=2903040, less than any 8-digit number.
     The digit factorial sum would similarly be less than any d-digit number for all d>8
     =#
-    for n in 10:9999999
+    for n in 10:(factorial(9)*7)
         if digit_factorial_sum(n) == n
             result += n
         end
