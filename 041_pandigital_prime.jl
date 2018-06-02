44a0a06e7ea2584925615ca98981dc68a89ccc21
SundaraRaman R <sundar@abiteasier.in>
2018-06-03 03:20:00 +0530

    041 optimize to check only potentially prime pandigitals
    17 microsec and 8.5 KB memory - more than 10000x better

--- 041_pandigital_prime.jl
+++ 041_pandigital_prime.jl
@@ -3,8 +3,8 @@ using Primes
 using Combinatorics 
 
 function largest_pandig_prime()
-    D = collect(9:-1:1)
-    while length(D) > 0
+    #only 7-, 4- and 1-digit pandigital numbers don't have digits adding up to something divisible by 3
+    for D in (collect(7:-1:1), collect(4:-1:1))
         #assumes permutations() switches from last to first, giving descending order here
         p = permutations(D)
         for nd in p
@@ -13,14 +13,10 @@ function largest_pandig_prime()
                 return n
             end
         end
-        popfirst!(D) #chop off largest digit so we can try n-1 digit numbers
     end
     -1
 end
 
 num_from_digits(dn) = sum([dn[k]*10^(k-1) for k=1:length(dn)])
 
-if VERSION ≤ v"0.6.3"
-    const popfirst! = shift!
-end
 isinteractive() || println(largest_pandig_prime())
