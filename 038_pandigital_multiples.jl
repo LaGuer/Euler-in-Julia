10f2f566299a97aa241dad6b9ee2a2a6d7aaae9d
SundaraRaman R <sundar@abiteasier.in>
2018-06-02 12:18:33 +0530

    038 comment and var name changes

--- 038_pandigital_multiples.jl
+++ 038_pandigital_multiples.jl
@@ -27,8 +27,9 @@ function largest_pandig_prod()
 
     For 4 digit long one: it can't start with any of 95-99, bcos that would lead to second part 
     starting 19, and 9 can't repeat in the pandigital number. 
-    So it starts with one of 91-94, and it's 5th and 6th digits are 18. Which means it starts with one of 
+    So it starts with one of 91-94, and its 5th and 6th digits are 18. Which means it starts with one of
     92-94 (since 1 can't repeat). 
+
     Starting 92, 93 or 94, then two digits, then 18, then 4 or 5, or 6 or 7, or 8 or 9.
     (184 or 185 for 92 since third digit after 92 can be <5 or ≥5. Similarly for 93,94.)
     Can't reuse 8 or 9, already used for 1st and 5th digits, so 94 is out too. 
@@ -44,11 +45,11 @@ function largest_pandig_prod()
         for d3 in allowed_d3
             allowed_d4 = allowed_d3[allowed_d3 .!= d3]
             for d4 in allowed_d4
-                i = 9000 + d2*100 + d3*10 + d4
+                prod1 = 9000 + d2*100 + d3*10 + d4
                 allowed_d678 = allowed_d4[allowed_d4 .!= d4]
-                prod2 = i*2
+                prod2 = prod1*2
                 if sort(digits(prod2), rev=true) == [8, allowed_d678..., 1]
-                    return i * 100000 + prod2
+                    return prod1 * 100000 + prod2
                 end
             end
         end
