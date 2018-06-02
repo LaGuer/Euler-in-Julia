3ad4da3895121f1727b5b5f78bb1adbfcce66742
SundaraRaman R <sundar@abiteasier.in>
2018-06-02 12:20:40 +0530

    009 have a b c be in ascending order, small readability changes

--- 009_spl_pyth_triplet.jl
+++ 009_spl_pyth_triplet.jl
@@ -11,11 +11,11 @@ Returns (-1, -1, -1) in case no triplet is found with the above properties.
 function spl_pyth_triplet(tripletsum = 1000)
     # a²+b² = c² => (a+b)² = c² + 2ab => a+b > c 
     # from that and a+b+c=tripletsum: c < tripletsum/2
-    c_max = floor(Int, tripletsum/2)
+    c_max = tripletsum÷2
     for c in c_max:-1:2
         csq = c^2
-        for a in c-1:-1:1
-            b = tripletsum - a - c
+        for b in (c-1):-1:1
+            a = tripletsum - b - c
             if a^2 + b^2 == csq
                 return (a, b, c)
             end
