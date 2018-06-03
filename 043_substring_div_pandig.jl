2d3333768c06de4638dbc4164cea03738b6a56c1
SundaraRaman R <sundar@abiteasier.in>
2018-06-03 18:19:17 +0530

    043 comment and docstring changes

--- 043_substring_div_pandig.jl
+++ 043_substring_div_pandig.jl
@@ -2,6 +2,20 @@
 using Base.Cartesian: @nloops
 const digits0to9 = Set(0:9)
 
+"""
+sum_subdiv_pandig()
+Problem statement:
+The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some order, but it also has a rather interesting sub-string divisibility property.
+Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note the following:
+    d2d3d4=406 is divisible by 2
+    d3d4d5=063 is divisible by 3
+    d4d5d6=635 is divisible by 5
+    d5d6d7=357 is divisible by 7
+    d6d7d8=572 is divisible by 11
+    d7d8d9=728 is divisible by 13
+    d8d9d10=289 is divisible by 17
+Find the sum of all 0 to 9 pandigital numbers with this property.
+"""
 function sum_subdiv_pandig()
     s = 0
     used_digits = Vector{Int}(10)
@@ -10,7 +24,7 @@ function sum_subdiv_pandig()
         @nloops(9, d,
                 (n -> allowed_digits_at(n, used_digits)), # range expr
                 (n -> used_digits[n] = d_n),              # pre-expr
-                begin                                     # loop body
+                begin                                     # innermost loop's body
                     sdpd_num = num_from_digits(reverse(used_digits))
                     s += sdpd_num
                 end)
