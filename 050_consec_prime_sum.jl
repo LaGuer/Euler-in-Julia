083e1573775a8caf1373c0970f9d9d510001c458
SundaraRaman R <sundar@abiteasier.in>
2018-06-05 03:24:23 +0530

    add 050 (half-century!) max-length consecutive prime sum

---
+++ 050_consec_prime_sum.jl
@@ -0,0 +1,32 @@
+
+using Primes
+
+"""
+max_consec_prime_sum([lim=1_000_000])
+Which prime, below one-million, can be written as the sum of the most consecutive primes?
+"""
+function max_consec_prime_sum(lim=1_000_000)
+    primenumbers = primes(lim-1)
+    starterp = maxl = maxconsecsum = 0
+    for (i, p) in enumerate(primenumbers)
+        consecsum = p
+        l = 1
+        if p == 2
+            consecsum += 3
+            l = 2
+        end
+        while consecsum < lim && (i + l + 1) < endof(primenumbers)
+            #at this point, consecsum is odd, and will remain odd (and hence potential prime)
+            # only after we add *two* odd numbers
+            consecsum += primenumbers[i + l] + primenumbers[i + l + 1]
+            l += 2
+            if l > maxl && consecsum in primenumbers
+                (starterp, maxl, maxconsecsum) = (p, l, consecsum)
+            end
+        end
+    end
+    (starterp, maxl, maxconsecsum)
+end
+
+isinteractive() || println(max_consec_prime_sum())
+
